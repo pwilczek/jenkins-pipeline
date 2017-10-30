7d138c9691d9a303944335f8d550f0d7ea06837d
Piotr <piotr.wilczek@wilu.name>
2017-10-30 21:13:47 +0100

    initial commit

---
+++ src/main/java/name/wilu/jenkins/pipeline/pipelineapp/PipelineAppApplication.java
@@ -0,0 +1,12 @@
+package name.wilu.jenkins.pipeline.pipelineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineAppApplication.class, args);
+	}
+}
